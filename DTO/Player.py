eb86a0c4b30e543e02843960457d4acbc040e815
mario <kugelblitz1337@yahoo.de>
2023-04-17 21:22:37 +0200

    Add Ship / Map

--- DTO/Player.py
+++ DTO/Player.py
@@ -1,5 +1,5 @@
 from DTO.Map import Map
-
+import re
 
 class Player:
 
@@ -20,15 +20,32 @@ class Player:
 
     def placeShips(self, coordinate):
         for name, length, count in self.__ships:
-            for i in range(1,count):
+            for i in range(1, count):
                 coordinate = input(f"Bitte geben Sie die Startkoordinate für das Schiff {name} an! z.B. B4")
                 orientation = input(f"Bitte geben Sie die Orientierung für das Schiff {name} an! z.B. N, O, S, W")
-                self.__validateCoordinate(coordinate)
+                splittedCoordinates = self.__validateCoordinate(coordinate)
+                validatedOrientation = self.__validateCoordinate(coordinate)
         input("Bitte geben Sie die Platzierung der Schiffe an:")
         self.__map.placeShips(coordinate)
 
-    def __validateCoordinate(self,coordinate):
-        print("Validiere Koordinate")
+    def __validateCoordinate(self, coordinate):
+        pattern = re.compile("^([a-jA-J])([1-9][0])?$")
+        matches = pattern.match(coordinate)
+        if matches:
+            return matches.groups()
+        else:
+            print("Die Eingabe der Koordinaten war nicht korrekt!")
+            #todo Exception werfen
+
+    def __validateOrientation(self, orientation):
+        pattern = re.compile("^[N,P,O,S]?$")
+        matches = pattern.match(orientation)
+        if matches:
+            return matches.string
+        else:
+            print("Die Eingabe der Orientierung war nicht korrekt!")
+            #todo Exception werfen
+
 
 
 
