7822b295aa659747ba2097bbdc7eace718373401
mario <kugelblitz1337@yahoo.de>
2023-05-02 10:09:04 +0200

    Tests fuer Field einbauen

---
+++ test/test_field.py
@@ -0,0 +1,20 @@
+#pylint: disable=C
+import unittest
+from DTO.field import Field
+
+
+class TestField(unittest.TestCase):
+
+    def test_get_field_hit(self):
+        field = Field(True, False)
+        self.assertTrue(field.get_field_hit())
+        field.fieldhit = False
+        self.assertFalse(field.get_field_hit())
+        field.fieldhit = False
+
+    def test_get_ship_on_field(self):
+        field = Field(False, True)
+        self.assertTrue(field.get_ship_on_field())
+        field.shiponfield = False
+        self.assertFalse(field.get_ship_on_field())
+        field.shiponfield = False
